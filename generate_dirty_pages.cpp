80da098a888f884ae862258982f57f5d84db3985
Faizul Bari <faizulbari@gmail.com>
2014-03-10 11:49:38 -0400

    modified:   generate_dirty_pages 	modified:   generate_dirty_pages.cpp

--- generate_dirty_pages.cpp
+++ generate_dirty_pages.cpp
@@ -56,19 +56,19 @@ int main(int argc, char *argv[])
 	// each second allocate dirty_rate amount of memory
 	// write one byte after each 4K bytes 
 	// uniformly distribute the writes over one second
+	double sleep_between_writes = (4.0 * 1000 * 1000) / dirty_rate; // sleep is in micro-seconds, 4.0 as we have 4 threads
+	struct timespec sleep_time;
+	sleep_time.tv_sec = sleep_between_writes / (1000 * 1000);
+	sleep_time.tv_nsec = (((int)sleep_between_writes) % (1000 * 1000)) * 1000;
+	//cout << "Info: sleep time (micro sec): " << sleep_between_writes << endl;
+	//cout << "Info: sleep time (sec): " << sleep_time.tv_sec << endl;
+	//cout << "Info: sleep time (nano sec): " << sleep_time.tv_nsec << endl;
+
 	while(true)
 	{
-		double sleep_between_writes = (4.0 * 1000 * 1000) / dirty_rate; // sleep is in micro-seconds, 4.0 as we have 4 threads
-		struct timespec sleep_time;
-		sleep_time.tv_sec = sleep_between_writes / (1000 * 1000);
-		sleep_time.tv_nsec = (((int)sleep_between_writes) % (1000 * 1000)) * 1000;
-		//cout << "Info: sleep time (micro sec): " << sleep_between_writes << endl;
-		//cout << "Info: sleep time (sec): " << sleep_time.tv_sec << endl;
-		//cout << "Info: sleep time (nano sec): " << sleep_time.tv_nsec << endl;
-
 		cout << "Info: Allocating " << dirty_rate * 4 *1024 << " Bytes" << endl;
 		void *mem_ptr;
-		mem_ptr = valloc(dirty_rate * 4 * 1024); // each memory page is of 4K bytes
+		mem_ptr = malloc(dirty_rate * 4 * 1024); // each memory page is of 4K bytes
 		if (mem_ptr == 0)
 		{
 			cout << "Error: Out of memory" << endl;
