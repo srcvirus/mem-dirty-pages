6babc51450518fff74a7f70384e9ce8fea769789
pWeb <pweb@vm4.defaultdomain>
2014-03-28 03:56:18 +0000

    new file:   udp-client.py

---
+++ udp-client.py
@@ -0,0 +1,14 @@
+import socket
+from time import sleep
+
+host = 'cn107' 
+port = 9999
+
+try:
+        s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error, msg:
+        print 'Failed to create socket. Error code: ', str(msg[0]), ' Message: ', msg[1]
+
+while 1:
+        s.sendto('heart-bit', (host, port))
+	sleep(0.5) # time is in seconds
